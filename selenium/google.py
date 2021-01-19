b0da0362fb50c7469ca517b312f878210049ca85
이지훈 <leejihoon@ijihun-ui-MacBookPro.local>
2021-01-20 00:00:30 +0900

    first commit

---
+++ selenium/google.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+
+executable_path = './chromedriver'
+driver = webdriver.Chrome(executable_path=executable_path)
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys("펜트하우스 주석경")
+elem.send_keys(Keys.RETURN)
+
+SCROLL_PAUSE_TIME = 1
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element_by_css_selector(".mye4qd").click()
+        except:
+            break
+    last_height = new_height
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+for image in images:
+    try:
+        image.click()
+        time.sleep(2)
+        imgurl = driver.find_element_by_xpath(
+            '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+        urllib.request.urlretrieve(imgurl, str(count)+".jpg")
+        count = count + 1
+    except:
+        pass
+
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+driver.close()
